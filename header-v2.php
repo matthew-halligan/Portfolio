b8ac4900febec9163822f76731ca3bda353a205e
matthew-halligan <42771186+matthew-halligan@users.noreply.github.com>
2018-10-03 14:06:52 -0400

    Add files via upload

---
+++ header-v2.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="assets/styles.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+</head>
+
+<body class="pagina">
+
+<header class="header">
+
+    <div class="header-image-container">
+        <img src="assets/img/Mushroom1.jpg" class="header-image" alt="Header Image">
+    </div>
+    <div class="header-text">
+        <div>
+            <div>
+                <span>Name</span>
+            </div>
+            <div>
+                <span>Location</span> | <span>Education</span>
+            </div>
+        </div>
+    </div>
+</header>
+
+<nav class="navbar">
+    <ul>
+            <li>
+                <a href="index.php">
+                    Home
+                </a>
+            </li>
+            <li>
+                <a href="work2.php">
+                    Work
+                </a>
+            </li>
+            <li>
+                <a href="about3.php">
+                    About
+                </a>
+            </li>
+            <li class="services">
+                <a href="service-list.php">
+                    Services
+                </a>
+            </li>
+            
+            <li>
+                <a href="contact.php">
+                    Contact Me
+                </a>
+            </li>
+        </ul>
+</nav>
---
+++ service-list.php
@@ -0,0 +1,52 @@
+<?php include 'header-v2.php'; ?>
+
+
+        <main class="main main-container flex-1 d-flex justify-content-center">
+            <div class="service-list-container">
+                
+                <div class="service-list-item-wrapper border-black">
+                    <div class="service-title border-black">
+                        Website Development and Redesign
+                    </div>
+                    <div class="service-description border-black">
+                        
+                        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid necessitatibus delectus nihil id dolorem praesentium nam, qui provident. Perferendis, hic magnam ratione ad inventore mollitia harum! A sunt excepturi laborum repudiandae omnis, itaque dolorem, sit suscipit deserunt totam blanditiis consectetur. Obcaecati quae itaque eum similique, non dolorem distinctio id possimus!</div>
+                    </div>
+                </div>
+
+                <div class="service-list-item-wrapper border-black">
+                    <div class="service-title border-black">
+                        Website Development and Redesign
+                    </div>
+                    <div class="service-description border-black">
+                         
+                        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid necessitatibus delectus nihil id dolorem praesentium nam, qui provident. Perferendis, hic magnam ratione ad inventore mollitia harum! A sunt excepturi laborum repudiandae omnis, itaque dolorem, sit suscipit deserunt totam blanditiis consectetur. Obcaecati quae itaque eum similique, non dolorem distinctio id possimus!</div>
+                    </div>
+                </div>
+
+
+                <div class="service-list-item-wrapper border-black">
+                    <div class="service-title border-black">
+                        Social Media Content and Interaction Management
+                    </div>
+                    <div class="service-description border-black">
+                         
+                        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid necessitatibus delectus nihil id dolorem praesentium nam, qui provident. Perferendis, hic magnam ratione ad inventore mollitia harum! A sunt excepturi laborum repudiandae omnis, itaque dolorem, sit suscipit deserunt totam blanditiis consectetur. Obcaecati quae itaque eum similique, non dolorem distinctio id possimus!</div>
+                    </div>
+                </div>
+
+                <div class="service-list-item-wrapper border-black">
+                    <div class="service-title border-black">
+                        Web Hosting Packages
+                    </div>
+                    <div class="service-description border-black">  
+                        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid necessitatibus delectus nihil id dolorem praesentium nam, qui provident. Perferendis, hic magnam ratione ad inventore mollitia harum! A sunt excepturi laborum repudiandae omnis, itaque dolorem, sit suscipit deserunt totam blanditiis consectetur. Obcaecati quae itaque eum similique, non dolorem distinctio id possimus!</div>
+                    </div>
+                </div>
+            </div>
+        </main>
+
+        <?php include 'footer.php'; ?>
+
+    </body>
+</html>
