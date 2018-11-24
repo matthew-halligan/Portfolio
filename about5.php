01580fae9e4de8379a9f4779cff37b892e74bd7e
Samuel Urias <msamuelurias@gmail.com>
2018-11-24 17:14:10 -0600

    Change to the about page to the new version

--- about5.php
+++ about5.php
@@ -8,60 +8,60 @@
 <?php include 'header-v2.php'; ?>
 <main class="flex-1 d-flex justify-content-center">
     <div class="about-container">
-      <div class="about-5-main-container">
+        <div class="about-5-main-container">
 
-          <!-- Picture -->
-          <div class="about-5-picture-container about-5-column">
-              <div class="about-5-picture-inner-container">
-                  <img src="assets/new_assets/About_page_pic.jpg"  class="about-picture-responsive-image" alt="About Picture">
-              </div>
-          </div>
+            <!-- Picture -->
+            <div class="about-5-picture-container about-5-column">
+                <div class="new-about-inner-container">
+                    <img src="assets/new_assets/About_page_pic.jpg"  class="about-picture-responsive-image" alt="About Picture">
+                </div>
+            </div>
 
-          <!-- Text -->
-          <div class="about-5-text-container about-5-column text-column">
-              <div class="">
-                  <div class="about-5-name">
-                      Matt Halligan
-                  </div>
-                  <div class="about-5-career">
-                      B.S. Of Computer Science
-                  </div>
-              </div>
+            <!-- Text -->
+            <div class="about-5-text-container about-5-column text-column about-5-column-right">
+                <div class="">
+                    <div class="about-5-name">
+                        Matt Halligan
+                    </div>
+                    <div class="about-5-career">
+                        B.S. Of Computer Science
+                    </div>
+                </div>
 
-              <div class="text">
-                  <p>As a student from Connecticut Studying Computer Science at Universtiy of Vermont, I've always had an interest in computers. I had been looking into web development for several months before coming to University and gave it a shot on FreeCodeCamp.org. Learning the basics of HTML and CSS gave me a good base prior to coming to Vermont and I was able to focus much more energy into learning php in my development classes.</p>
-              </div>
+                <div class="text">
+                    <p>As a student from Connecticut Studying Computer Science at Universtiy of Vermont, I've always had an interest in computers. I had been looking into web development for several months before coming to University and gave it a shot on FreeCodeCamp.org. Learning the basics of HTML and CSS gave me a good base prior to coming to Vermont and I was able to focus much more energy into learning php in my development classes.</p>
+                </div>
 
-              <div class="text">
-                  <p>These first two years of classes have given me the tools that I need to make any project idea come into a reality. This confidence has been the base to my mission of growing your business into the online world.</p>
-              </div>
+                <div class="text">
+                    <p>These first two years of classes have given me the tools that I need to make any project idea come into a reality. This confidence has been the base to my mission of growing your business into the online world.</p>
+                </div>
 
-              <div class="about-5-bullet-points-container">
-                  <div class="about-5-bullet-subtitle-container">
-                      <h2 class="about-5-bullet-subtitle">Academics</h2>
-                  </div>
-
-
-                  <!-- Cards elements -->
-                  <div class="about-5-bullet-text text">
-                      <p>B.S. Of Computer Science University of Vermont '21</p>
-                  </div>
-              </div>
+                <div class="about-5-bullet-points-container">
+                    <div class="about-5-bullet-subtitle-container">
+                        <h2 class="about-5-bullet-subtitle">Academics</h2>
+                    </div>
 
 
-              <div class="about-5-bullet-points-container">
-                  <div class="about-5-bullet-subtitle-container">
-                      <h2 class="about-5-bullet-subtitle">Certifications</h2>
-                  </div>
+                    <!-- Cards elements -->
+                    <div class="about-5-bullet-text text">
+                        <p>B.S. Of Computer Science University of Vermont '21</p>
+                    </div>
+                </div>
 
 
-                  <!-- Cards elements -->
-                  <div class="about-5-bullet-text text">
-                      <p>4/5 complete on University of Vermont Cyber-Security Certification</p>
-                  </div>
-              </div>
-          </div>
-      </div>
+                <div class="about-5-bullet-points-container">
+                    <div class="about-5-bullet-subtitle-container">
+                        <h2 class="about-5-bullet-subtitle">Certifications</h2>
+                    </div>
+
+
+                    <!-- Cards elements -->
+                    <div class="about-5-bullet-text text">
+                        <p>4/5 complete on University of Vermont Cyber-Security Certification</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </main>
 
